9f0ff799b37ad559781bedc6bd4e9f8b882f54c4
JH <53140919+FreemanRyan@users.noreply.github.com>
2022-11-29 14:10:45 +0800

    Update index.js

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ function generateProgressBar() {
 }
 
 const readme = `\
-### Hi JH 👋
+### Hi there 👋
 ⏳ Year progress ${progressBarOfThisYear} ${(progressOfThisYear * 100).toFixed(2)} %
 ---
 ⏰ Updated on ${new Date().toUTCString()}
